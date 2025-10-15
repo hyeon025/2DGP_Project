e1e6c2f4f079ce12437e7a29ae3998d3270b105f
hyeon025 <kjh04255@naver.com>
2025-10-15 22:58:58 +0900

    메인함수 기본 틀 준비

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from pico2d import *
 
+def reset_canvas():
+    pass
+
+
+def handle_events():
+    pass
+
+def update_map():
+    pass
+
+
+def render_world():
+    pass
+
 open_canvas()
 
-while True:
-    pass
+reset_canvas()
+
+Play = True
+
+while Play:
+    handle_events()
+
+    update_map()
+
+    render_world()
+
+    delay(0.05)
 
 
 close_canvas()
