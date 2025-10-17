e67bb0e49e9e43ebd9e46088cb77e40c9f5ff566
hyeon025 <kjh04255@naver.com>
2025-10-17 22:20:29 +0900

    idle 상태 추가

--- player.py
+++ player.py
@@ -1,11 +1,29 @@
 from pico2d import load_image
 
 
+class Idle:
+    def __init__(self, player):
+        self.player = player
+
+    def enter(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def do(self):
+        pass
+
+    def draw(self):
+        self.player.job.clip_draw(0, 80, 40, 40, self.player.x, self.player.y, 80, 80)
+
 class Player:
     def __init__(self, job):
         self.x = 600
         self.y = 300
         self.job = load_image(job)
+
+        self.IDLE = Idle(self)
 
     def draw(self):
         self.job.clip_draw(0, 80, 40, 40, self.x, self.y, 80, 80)
