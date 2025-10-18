494d28b132edb299385bb0649c7e6e1db4b11b01
hyeon025 <kjh04255@naver.com>
2025-10-18 15:08:20 +0900

    방향키 입력시 방향 결정 방식 변경

--- player.py
+++ player.py
@@ -20,111 +20,81 @@ def s_down(e):
 def s_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_s
 
-class Walk_xy:
+class Walk:
     def __init__(self, player):
         self.player = player
 
-    def enter(self,e):
+    def enter(self, e):
+        self.update_key_and_dir(e)
+
+    def exit(self, e):
+        pass
+
+    def do(self):
+        self.player.frame = (self.player.frame + 1) % 8
+        self.player.x += self.player.dir_x * 0.5
+        self.player.y += self.player.dir_y * 0.5
+
+        if not any(self.player.keys.values()) or (self.player.dir_x == 0 and self.player.dir_y == 0):
+            self.player.state_machine.current_state = self.player.IDLE
+            self.player.IDLE.enter(('STOP', 0))
+
+    def draw(self):
+        if self.player.face_dir == 1:
+            self.player.job.clip_draw(self.player.frame * 40, 40, 40, 40, self.player.x, self.player.y, 80, 80)
+        else:
+            self.player.job.clip_composite_draw(self.player.frame * 40, 40, 40, 40, 0,'h', self.player.x, self.player.y, 80, 80)
+
+    def update_key_and_dir(self, e):
+        # 키 상태 업데이트
         if d_down(e):
-            self.player.dir_x += 1
-            self.player.face_dir = 1
+            self.player.keys['d'] = True
         elif d_up(e):
-            self.player.dir_x -= 1
+            self.player.keys['d'] = False
         elif a_down(e):
-            self.player.dir_x -= 1
-            self.player.face_dir = -1
+            self.player.keys['a'] = True
         elif a_up(e):
-            self.player.dir_x += 1
+            self.player.keys['a'] = False
         elif w_down(e):
-            self.player.dir_y += 1
+            self.player.keys['w'] = True
         elif w_up(e):
-            self.player.dir_y -= 1
+            self.player.keys['w'] = False
         elif s_down(e):
-            self.player.dir_y -= 1
+            self.player.keys['s'] = True
         elif s_up(e):
-            self.player.dir_y += 1
+            self.player.keys['s'] = False
 
-    def exit(self,e):
-        pass
+        self.player.dir_x = 0
+        self.player.dir_y = 0
 
-    def do(self):
-        self.player.frame = (self.player.frame + 1) % 8
-        self.player.x += self.player.dir_x * 0.5
-        self.player.y += self.player.dir_y * 0.5
-
-    def draw(self):
-        if self.player.face_dir == 1:
-            self.player.job.clip_draw(self.player.frame * 40, 40, 40, 40, self.player.x, self.player.y, 80, 80)
-        else:
-            self.player.job.clip_composite_draw(self.player.frame * 40, 40, 40, 40, 0,'h', self.player.x, self.player.y, 80, 80)
-
-
-class Walk_y:
-    def __init__(self, player):
-        self.player = player
-
-    def enter(self,e):
-        if w_down(e):
-            self.player.dir_y += 1
-        elif w_up(e):
-            self.player.dir_y -= 1
-        elif s_down(e):
-            self.player.dir_y -= 1
-        elif s_up(e):
-            self.player.dir_y += 1
-
-    def exit(self,e):
-        pass
-
-    def do(self):
-        self.player.frame = (self.player.frame + 1) % 8
-        self.player.y += self.player.dir_y * 0.5
-
-    def draw(self):
-        if self.player.face_dir == 1:
-            self.player.job.clip_draw(self.player.frame * 40, 40, 40, 40, self.player.x, self.player.y, 80, 80)
-        else:
-            self.player.job.clip_composite_draw(self.player.frame * 40, 40, 40, 40, 0,'h', self.player.x, self.player.y, 80, 80)
-
-
-class Walk_x:
-    def __init__(self, player):
-        self.player = player
-
-    def enter(self,e):
-        if d_down(e):
+        if self.player.keys['d']:
             self.player.dir_x += 1
+        if self.player.keys['a']:
+            self.player.dir_x -= 1
+        if self.player.keys['w']:
+            self.player.dir_y += 1
+        if self.player.keys['s']:
+            self.player.dir_y -= 1
+
+        if self.player.dir_x > 0:
             self.player.face_dir = 1
-        elif d_up(e):
-            self.player.dir_x -= 1
-        elif a_down(e):
-            self.player.dir_x -= 1
+        elif self.player.dir_x < 0:
             self.player.face_dir = -1
-        elif a_up(e):
-            self.player.dir_x += 1
 
-    def exit(self,e):
-        pass
-
-    def do(self):
-        self.player.frame = (self.player.frame + 1) % 8
-        self.player.x += self.player.dir_x * 0.5
-
-    def draw(self):
-        if self.player.face_dir == 1:
-            self.player.job.clip_draw(self.player.frame * 40, 40, 40, 40, self.player.x, self.player.y, 80, 80)
-        else:
-            self.player.job.clip_composite_draw(self.player.frame * 40, 40, 40, 40, 0,'h', self.player.x, self.player.y, 80, 80)
 
 class Idle:
     def __init__(self, player):
         self.player = player
 
-    def enter(self,e):
+    def enter(self, e):
+        if e[0] == 'INPUT':
+            if d_up(e) or a_up(e) or w_up(e) or s_up(e):
+                self.update_key_and_dir(e)
+                return
         self.player.dir_x = 0
         self.player.dir_y = 0
 
-    def exit(self,e):
+    def exit(self, e):
         pass
 
     def do(self):
@@ -132,9 +102,36 @@ class Idle:
 
     def draw(self):
         if self.player.face_dir == 1:
-            self.player.job.clip_draw(self.player.frame * 40, 80, 40, 40, self.player.x, self.player.y, 80, 80)
+            self.player.job.clip_draw(self.player.frame * 40, 80, 40, 40,self.player.x, self.player.y, 80, 80)
         else:
-            self.player.job.clip_composite_draw(self.player.frame * 40, 80, 40, 40, 0,'h', self.player.x, self.player.y, 80, 80)
+            self.player.job.clip_composite_draw(self.player.frame * 40, 80, 40, 40,0, 'h', self.player.x, self.player.y, 80, 80)
+
+    def update_key_and_dir(self, e):
+        if d_up(e):
+            self.player.keys['d'] = False
+        elif a_up(e):
+            self.player.keys['a'] = False
+        elif w_up(e):
+            self.player.keys['w'] = False
+        elif s_up(e):
+            self.player.keys['s'] = False
+
+        self.player.dir_x = 0
+        self.player.dir_y = 0
+
+        #키 상태 체크
+        if self.player.keys['d']:
+            self.player.dir_x += 1
+        if self.player.keys['a']:
+            self.player.dir_x -= 1
+        if self.player.keys['w']:
+            self.player.dir_y += 1
+        if self.player.keys['s']:
+            self.player.dir_y -= 1
+
+        if self.player.dir_x != 0 or self.player.dir_y != 0:
+            self.player.state_machine.current_state = self.player.WALK
+            self.player.WALK.enter(e)
 
 class Player:
     def __init__(self, job):
@@ -146,24 +143,19 @@ class Player:
         self.dir_y = 0
         self.frame = 0
 
+        self.keys = {'d': False, 'a': False, 'w': False, 's': False}
+
         self.IDLE = Idle(self)
-        self.WALK_X = Walk_x(self)
-        self.WALK_Y = Walk_y(self)
-        self.WALK_XY = Walk_xy(self)
+        self.WALK = Walk(self)
 
         self.state_machine = StateMachine(
             self.IDLE,
             {
-            self.IDLE:{d_down: self.WALK_X, a_down: self.WALK_X, w_down: self.WALK_Y, s_down: self.WALK_Y
-                       , d_up:  self.WALK_X, a_up:  self.WALK_X, w_up:  self.WALK_Y, s_up: self.WALK_Y},
-            self.WALK_X:{d_up: self.IDLE, a_up: self.IDLE, w_up: self.WALK_XY, s_up: self.WALK_XY,
-                        d_down: self.IDLE, a_down: self.IDLE, w_down: self.WALK_XY, s_down: self.WALK_XY},
-            self.WALK_Y:{w_up: self.IDLE, s_up: self.IDLE, d_up: self.WALK_XY, a_up: self.WALK_XY,
-                         w_down: self.IDLE, s_down: self.IDLE, d_down: self.WALK_XY,a_down: self.WALK_XY},
-            self.WALK_XY:{d_up: self.WALK_Y, a_up: self.WALK_Y, w_up: self.WALK_X, s_up: self.WALK_X,
-                          d_down: self.WALK_Y, a_down: self.WALK_Y, w_down: self.WALK_X, s_down: self.WALK_X}
+                self.IDLE: {d_down: self.WALK,a_down: self.WALK,w_down: self.WALK,s_down: self.WALK,
+                            d_up: self.WALK, a_up: self.WALK,w_up: self.WALK, s_up: self.WALK},
+                self.WALK: {d_down: self.WALK, d_up: self.WALK,a_down: self.WALK, a_up: self.WALK,
+                            w_down: self.WALK, w_up: self.WALK,s_down: self.WALK, s_up: self.WALK}
             })
-
 
     def draw(self):
         self.state_machine.draw()
