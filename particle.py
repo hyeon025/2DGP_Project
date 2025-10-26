ee110d13bda9619c901561cb181c9c2b81eb2d1c
hyeon025 <kjh04255@naver.com>
2025-10-27 08:21:45 +0900

    파티클 클래스 만들기

--- particle.py
+++ particle.py
@@ -1,2 +1,10 @@
+from pico2d import load_image
+
 class Particle:
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = load_image('asset/Particle/atf_star.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y, 40, 40)
