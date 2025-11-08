fa2723966970efe7b92ffaccf8de479b9cd2e318
hyeon025 <kjh04255@naver.com>
2025-11-08 14:55:12 +0900

    월드좌표와 카메라 좌표 정의

---
+++ camera.py
@@ -0,0 +1,16 @@
+class Camera:
+    def __init__(self, world_w, world_h, screen_w, screen_h):
+        self.world_w = world_w
+        self.world_h = world_h
+        self.screen_w = screen_w
+        self.screen_h = screen_h
+        self.x = 0
+        self.y = 0
+
+    def update(self, x, y):
+        cx = x - self.screen_w // 2 #중심
+        cy = y - self.screen_h // 2
+
+        cx = max(0, min(cx, self.world_w - self.screen_w))
+        cy = max(0, min(cy, self.world_h - self.screen_h))
+        self.x, self.y = cx, cy
