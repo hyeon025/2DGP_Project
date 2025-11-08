0c665eef0a9187feabf470f426d83122ad636ab7
hyeon025 <kjh04255@naver.com>
2025-11-08 15:56:11 +0900

    1라운드 모드 파일 함수 만들기

---
+++ round_1_mode.py
@@ -0,0 +1,24 @@
+from pico2d import *
+
+from camera import Camera
+
+camera = Camera(world_w=5000, world_h=5000, screen_w=1200, screen_h=900)
+
+
+def init():
+    pass
+
+def handle_events():
+    pass
+
+def update():
+    pass
+
+def draw():
+    pass
+
+def finish():
+    pass
+
+def pause(): pass
+def resume(): pass
