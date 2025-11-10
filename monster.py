6fec3532a605a3f750c9318c94ca6c24c2654d92
hyeon025 <kjh04255@naver.com>
2025-11-11 03:08:21 +0900

    몬스터 클래스 만들기

---
+++ monster.py
@@ -0,0 +1,17 @@
+class Monster:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def handle_collision(self, group, other):
+        pass
+
+    def get_bb(self):
+        pass
+
+
