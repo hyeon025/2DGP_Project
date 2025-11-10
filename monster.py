f0a644bde65a24a312065ae0753e721b75d65eb0
hyeon025 <kjh04255@naver.com>
2025-11-11 03:10:59 +0900

    몬스터 기본 객체 변수 만들기

--- monster.py
+++ monster.py
@@ -1,6 +1,12 @@
 class Monster:
-    def __init__(self):
-        pass
+    def __init__(self,x,y,hp):
+        self.x = x
+        self.y = y
+        self.frame = 0
+        self.face_dir = 1
+        self.hp = hp
+        self.alive = True
+
 
     def update(self):
         pass
